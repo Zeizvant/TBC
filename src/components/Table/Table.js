22b6001be22fe6a95fa57b24678f194749e0a462
admin <admin@admins-MacBook-Pro-2.local>
2023-02-25 11:03:45 +0400

    initial commit

---
+++ src/components/Table/Table.js
@@ -0,0 +1,11 @@
+import './index.css'
+import MUIDataTable from "mui-datatables";
+import AddIcon from '@mui/icons-material/Add';
+
+export const Table = () => {
+    return (
+        <div className='table'>
+
+        </div>
+    )
+}
