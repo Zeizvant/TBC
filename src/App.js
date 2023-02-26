3e6fd02ca6d92e836344fa39609093f1fb4a6729
admin <admin@admins-MacBook-Pro-2.local>
2023-02-26 17:22:57 +0400

    added input form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { Form } from './components/Form/Form';
 import { Table } from './components/Table/Table';
 
 function App() {
 
   return (
     <div className="container">
-      <Table />
+      <Form />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,27 @@
 import './index.css'
+import CloseIcon from '@mui/icons-material/Close';
 
 export const Form = () => {
     return (
         <div className="form">
-
+            <div className='header'>
+                <h1 className='form-header'>ველის დამატება</h1>
+                <CloseIcon className='close-btn'/>
+            </div>
+            <input type='text' placeholder='სახელი'/>
+            <input type='text' placeholder='გვარი'/>
+            <input type='text' placeholder='პირადი ნომერი'/>
+            <div className='date'>
+                <label>დაბადების თარიღი</label>
+                <input type='date' />   
+            </div>
+            <select name='gender' id='gender'>
+                <option calue='male'>Male</option>
+                <option calue='female'>Female</option>
+            </select>
+            <input type='text' placeholder='დაბადების ადგილი'/>
+            <input type='text' placeholder='მისამართი'/>
+            <button>შენახვა</button>
         </div>
     )
 }
