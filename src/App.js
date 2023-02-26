ba0d49e22253bc2fff9bddac46bc06024fc9fe31
admin <admin@admins-MacBook-Pro-2.local>
2023-02-26 16:33:46 +0400

    added table structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Table } from './components/Table/Table';
 
 function App() {
 
   return (
     <div className="container">
-      
+      <Table />
     </div>
   );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,9 +3,26 @@ import MUIDataTable from "mui-datatables";
 import AddIcon from '@mui/icons-material/Add';
 
 export const Table = () => {
+
+    const columns = ["სახელი", "გვარი", "პირადი ნომერი", "სქესი", "დაბადების თარიღი", "დაბადების ადგილი", "მისმართი"];
+    const data = [
+    ];
+       
+    const options = {
+        filterType: 'checkbox',
+    };
+       
+       
     return (
         <div className='table'>
-
+            <MUIDataTable
+                data={data}
+                columns={columns}
+                options={{
+                    ...options,
+                    customToolbar: () => (<AddIcon aria-label='Add' id='plus-button' className='MuiButtonBase-root MuiIconButton-root tss-10rusft-MUIDataTableToolbar-icon MuiIconButton-sizeMedium css-78trlr-MuiButtonBase-root-MuiIconButton-root' />),
+                  }}
+            />
         </div>
     )
 }
